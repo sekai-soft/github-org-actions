a31a32b9431670833890f59caf674a5302aaf09e
KTachibanaM <whj19931115@gmail.com>
2024-11-07 10:42:35 -0800

    add index & deploy to fly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
 from typing import Annotated
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
+from fastapi.templating import Jinja2Templates
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from github import Github, Auth
 from github_org_actions.models import RepoResult
@@ -8,22 +10,58 @@ from github_org_actions.github import get_repo_res
 
 class Settings(BaseSettings):
     github_token: str
+    ui_default_org: str
+    ui_default_excluded_repos: str
     model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 auth = Auth.Token(settings.github_token)
 github = Github(auth=auth)
 
 
-@app.get("/api/{org}")
-async def _api(org: str, e: Annotated[list[str], Query()] = []) -> list[RepoResult]:
+def _get_res(org: str, excluded_repos: list[str]) -> list[RepoResult]:
     org = github.get_organization(org)
     repos = org.get_repos(type="public")
     res = []
     for repo in repos:
-        repo_res = get_repo_res(repo, e)
+        repo_res = get_repo_res(repo, excluded_repos)
         if repo_res:
             res.append(repo_res)
     return res
+
+
+def time_ago(timestamp):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    diff = now - timestamp
+    seconds = diff.total_seconds()
+    
+    if seconds >= 31536000:
+        return f"{int(seconds // 31536000)}y ago"
+    elif seconds >= 2592000:
+        return f"{int(seconds // 2592000)}m ago"
+    elif seconds >= 86400:
+        return f"{int(seconds // 86400)}d ago"
+    elif seconds >= 3600:
+        return f"{int(seconds // 3600)}h ago"
+    elif seconds >= 60:
+        return f"{int(seconds // 60)}m ago"
+    else:
+        return f"{int(seconds)}s ago"
+
+
+@app.get("/")
+async def _root(request: Request):
+    res = _get_res(settings.ui_default_org, settings.ui_default_excluded_repos.split(","))
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"res": res, "time_ago": time_ago}
+    )
+
+
+@app.get("/api/{org}")
+async def _api(org: str, e: Annotated[list[str], Query()] = []) -> list[RepoResult]:
+    return _get_res(org, e)
